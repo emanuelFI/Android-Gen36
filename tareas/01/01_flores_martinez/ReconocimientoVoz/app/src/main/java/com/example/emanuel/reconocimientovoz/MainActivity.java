cb8d1c65b00e63cc86c69eee5daf28e6f345a47a
emanuelFI <muf-pc>
2018-05-09 04:21:29 -0500

    Actualizacion Voz

--- tareas/01/01_flores_martinez/ReconocimientoVoz/app/src/main/java/com/example/emanuel/reconocimientovoz/MainActivity.java
+++ tareas/01/01_flores_martinez/ReconocimientoVoz/app/src/main/java/com/example/emanuel/reconocimientovoz/MainActivity.java
@@ -6,13 +6,19 @@ import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    TextView grabar;
+    //TextView grabar;
+    private GridView gridView;
+    private GridAdapter adapter;
+    ImageButton button;
+    ArrayList<String> conjunto = new ArrayList<>();
 
     private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
 
@@ -21,12 +27,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        grabar = (TextView) findViewById(R.id.txtGrabarVoz);
+        button = (ImageButton) findViewById(R.id.img_btn_hablar);
+
+        gridView = (GridView) findViewById(R.id.gridViewText);
+        adapter = new GridAdapter(this, conjunto);
+
+        //grabar = (TextView) findViewById(R.id.txtGrabarVoz);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentActionRecognizeSpeech = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+                intentActionRecognizeSpeech.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-MX");
+                try {
+                    startActivityForResult(intentActionRecognizeSpeech, RECOGNIZE_SPEECH_ACTIVITY);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(), "Tú dispositivo no soporta el reconocimiento por voz", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
@@ -36,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
 
                     ArrayList<String> speech = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String strSpeech2Text = speech.get(0);
-
-                    grabar.setText(strSpeech2Text);
+                    conjunto.add(strSpeech2Text);
+                    gridView.setAdapter(adapter);
                 }
 
                 break;
@@ -45,19 +68,5 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
-    }
-
-    public void onClickImgBtnHablar(View v) {
-
-        Intent intentActionRecognizeSpeech = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-        intentActionRecognizeSpeech.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-MX");
-        try {
-            startActivityForResult(intentActionRecognizeSpeech,
-                    RECOGNIZE_SPEECH_ACTIVITY);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(), "Tú dispositivo no soporta el reconocimiento por voz", Toast.LENGTH_SHORT).show();
-        }
-
     }
 }
